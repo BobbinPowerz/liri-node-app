f2004b11f80b49470a86e9ae70730576d7d050f6
unknown <bobbin.powerz@gmail.com>
2017-10-10 23:09:53 -0700

    added switch statement and connected 3 parts

--- liri.js
+++ liri.js
@@ -5,28 +5,38 @@
 	var Twitter = require('twitter');
 	var Spotify = require ('node-spotify-api');
 
-	// function myTweets() {
-	// 	var client = new Twitter(keys);
-	// 	params = {screen_name: 'AlexZ84775393', count: 20 };
-	// 	client.get("statuses/user_timeline/", params, function(error, data, response){
-	// 		if (!error) { 
-	// 			//console.log(args); // full data list
-	// 			 for(var i = 0; i < data.length; i++) {
-	// 			 	var tweetsText = data[i].text +"\n" + "Created on " + data[i].created_at + "\n";
-	// 				console.log(tweetsText);
-	// 			}
-	// 		}  else {
-	// 			console.log("Error :"+ error);
-	// 			return;
-	// 		}
-	// 	});
-	// };
 
-	// myTweets();
+	var action = process.argv[2];
+	switch(action) {
+		case "my-tweets": myTweets(); 
+		break;
+		case "spotify-this-song": spotifyThisSong();
+		break;
+		case "movie-this": movieThis();
+		break;
+		// case "do-what-it-says": doWhatItSays(); break;		
+	};
+
+	function myTweets() {
+		var client = new Twitter(keys.twitter);
+		params = {screen_name: 'AlexZ84775393', count: 20 };
+		client.get("statuses/user_timeline/", params, function(error, data, response){
+			if (!error) { 
+				//console.log(data); 
+				 for(var i = 0; i < data.length; i++) {
+				 	var tweetsText = data[i].text +"\n" + "Created on " + data[i].created_at + "\n";
+					console.log(tweetsText);
+				}
+			}  else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
+	};
 
 	function spotifyThisSong(song) {
-		var spotify = new Spotify(keys);
-		var song = process.argv[2];
+		var spotify = new Spotify(keys.spotify);
+		var song = process.argv[3];
 		if(!song){
 			song = "The Sign";
 		}
@@ -35,47 +45,42 @@
 			if(!err){
 				var songData = data.tracks.items;
 					if (songData[0] != undefined) {
-						var responseBack =
-						"Artist: " + songData[0].artists[0].name + "\n" +
+						var responseBack = "Artist: " + songData[0].artists[0].name + "\n" +
 						"Song: " + songData[0].name + "\n" +
 						"Preview Url: " + songData[0].preview_url + "\n" +						
 						"Album the song is from: " + songData[0].album.name +"\n";
 						console.log(responseBack);
 					}
 			}	else {
-				console.log("Error :"+ err);
+				console.log("Error :" + err);
 				return;
 			}
 		});
 	};
 
-	spotifyThisSong();
+	function movieThis(){
+		var movie = process.argv[3];
+		if(!movie){
+			movie = "mr nobody";
+		}
+		params = movie
+		request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var movieBody = JSON.parse(body);
+				var toDisplay = "Title: " + movieBody.Title +"\n"+
+				"Year: " + movieBody.Year + "\n"+
+				"Imdb Rating: " + movieBody.imdbRating + "\n"+
+				"Rotten Tomatoes Rating: " + movieBody.Ratings[1].Value +"\n"+
+				"Country: " + movieBody.Country +"\n"+
+				"Language: " + movieBody.Language +"\n"+
+				"Plot: " + movieBody.Plot +"\n"+
+				"Actors: " + movieBody.Actors +"\n";
+				console.log(toDisplay);
+			} else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
+	};
 
-	// function movieThis(){
-	// 	var movie = process.argv[2];
-	// 	if(!movie){
-	// 		movie = "mr nobody";
-	// 	}
-	// 	params = movie
-	// 	request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
-	// 		if (!error && response.statusCode == 200) {
-	// 			var movieBody = JSON.parse(body);
-	// 			//console.log(movieObject); 
-	// 			var toDisplay = "Title: " + movieBody.Title +"\n"+
-	// 			"Year: " + movieBody.Year + "\n"+
-	// 			"Imdb Rating: " + movieBody.imdbRating + "\n"+
-	// 			"Rotten Tomatoes Rating: " + movieBody.Ratings[1].Value +"\n"+
-	// 			"Country: " + movieBody.Country +"\n"+
-	// 			"Language: " + movieBody.Language +"\n"+
-	// 			"Plot: " + movieBody.Plot +"\n"+
-	// 			"Actors: " + movieBody.Actors +"\n";
-	// 			console.log(toDisplay);
-	// 		} else {
-	// 			console.log("Error :"+ error);
-	// 			return;
-	// 		}
-	// 	});
-	// };
-
-	// movieThis();
 	
